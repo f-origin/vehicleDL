06e22e663acbc799c3f0dc8401fd9a3c0a1749af
origin <f_origin@163.com>
2019-02-12 21:29:32 +0800

    update

--- code/slim/datasets/dataset_factory.py
+++ code/slim/datasets/dataset_factory.py
@@ -22,12 +22,14 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
+from datasets import pj_vehicle
 
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
     'imagenet': imagenet,
     'mnist': mnist,
+    'pj_vehicle': pj_vehicle
 }
 
 
