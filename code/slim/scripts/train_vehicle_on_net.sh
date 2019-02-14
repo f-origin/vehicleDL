4090b0a896d4d25d0f0038017a4e7f4e5612e85b
origin <f_origin@163.com>
2019-02-14 22:15:44 +0800

    update

--- code/slim/scripts/train_vehicle_on_net.sh
+++ code/slim/scripts/train_vehicle_on_net.sh
@@ -26,52 +26,27 @@ set -e
 
 echo "run train vehicle shell"
 # Where the checkpoint and logs will be saved to.
-OUT_DIR=/output
-# OUT_DIR=~/tmp
+# OUT_DIR=/output
+OUT_DIR=~/tmp
 
 TRAIN_DIR=${OUT_DIR}/vehicle-model
 
 # Where the dataset is saved to.
-DATASET_DIR=/data/forigin/car-detction
-# DATASET_DIR=~/tmp/vehicle
+# DATASET_DIR=/data/forigin/car-detction
+DATASET_DIR=~/tmp/vehicle
 
 # Model name
 MODEL_NAME=inception_v4
 
-# Run training.
-python3 train_image_classifier.py \
-  --train_dir=${TRAIN_DIR} \
-  --dataset_name=pj_vehicle \
-  --dataset_split_name=train \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=${MODEL_NAME} \
-  --checkpoint_path=${DATASET_DIR}/inception_v4.ckpt \
-  --checkpoint_exclude_scopes=InceptionV4/Logits,InceptionV4/AuxLogits \
-  --preprocessing_name=inception \
-  --max_number_of_steps=5000 \
-  --batch_size=15 \
-  --save_interval_secs=120 \
-  --save_summaries_secs=120 \
-  --log_every_n_steps=100 \
-  --optimizer=adam \
-  --learning_rate=1 \
-  --num_epochs_per_decay=200 \
-  --weight_decay=0.004
 
-# Run evaluation.
-python3 eval_image_classifier.py \
-  --checkpoint_path=${TRAIN_DIR} \
-  --eval_dir=${TRAIN_DIR} \
-  --dataset_name=pj_vehicle \
-  --dataset_split_name=validation \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=${MODEL_NAME}
 
 # Run export.
-#python3 export_inference_graph.py \
-#  --alsologtostderr \
-#  --model_name=${MODEL_NAME} \
-#  --output_file=${TRAIN_DIR}/inception_v3_inf_graph.pb
+python3 export_inference_graph.py \
+  --model_name=${MODEL_NAME} \
+  --batch_size=1 \
+  --dataset_name=pj_vehicle \
+  --dataset_dir=${DATASET_DIR} \
+  --output_file=${TRAIN_DIR}/inception_v3_inf_graph.pb
 
 #cd ${OUT_DIR}
 #tar -zvcf model_exported.tar.gz vehicle-model/
