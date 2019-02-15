c9df96d5957da685dab7d9c3ae91d7961fab8744
f-origin <f_origin@163.com>
2019-02-15 14:58:56 +0800

    Update train_vehicle_on_net.sh

--- code/slim/scripts/train_vehicle_on_net.sh
+++ code/slim/scripts/train_vehicle_on_net.sh
@@ -54,14 +54,13 @@ python3 train_image_classifier.py \
   --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
   --preprocessing_name=inception \
   --max_number_of_steps=5000 \
-  --batch_size=50 \
+  --batch_size=50 \
+  --learning_rate=0.1 \
   --save_interval_secs=120 \
   --save_summaries_secs=120 \
   --log_every_n_steps=100 \
-  --learning_rate=0.5 \
-  --clone_on_cpu=False \
-  --batch_size=50 \
-  --learning_rate=0.01 \
+  --clone_on_cpu=False 
+  
 
 # Run evaluation.
 python3 eval_image_classifier.py \
