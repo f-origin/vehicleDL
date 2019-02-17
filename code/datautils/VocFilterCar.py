4fa0c56b68b1268a5354352ca8a6d91d26310584
origin <f_origin@163.com>
2019-02-17 18:54:55 +0800

    update

--- code/datautils/VocFilterCar.py
+++ code/datautils/VocFilterCar.py
@@ -1,42 +1,40 @@
-"""
-解析使用xml.etree.ElementTree 模块，生成使用xml.dom.minidom模块，
-ElementTree比dom快，dom生成简单且会自动格式化
-"""
-import xml.dom.minidom as minidom
 import xml.etree.ElementTree as ET
 import os
-import sys
 
 xmlPath = r'F:\PycharmProjects\VOC2012\Annotations'
 jpgPath = r'F:\PycharmProjects\VOC2012\JPEGImages'
 
 def filterXml(fliePath,jpgPath):
-    xmlFilePath = os.path.abspath(fliePath)
-    # 是否删除xml 和对应的图片
-    # 如果没有car标注的话删除xml 和 对应的 jpg
-    flag = True
+    try:
+        xmlFilePath = os.path.abspath(fliePath)
+        # 是否删除xml 和对应的图片
+        # 如果没有car标注的话删除xml 和 对应的 jpg
+        flag = True
 
-    tree = ET.parse(xmlFilePath)
-    # 获得根节点
-    root = tree.getroot()
-    jpgName = root[1].text
-    for child in root:
-        if child.tag == 'object' and child[0].text == 'sheep':
-            flag = False
-
-    if flag:
-        print('不存在 car标注，删除 %s 和 对应的图片%s' % (xmlFilePath, jpgName))
-        os.remove(xmlFilePath)
-        os.remove(os.path.join(jpgPath, jpgName))
-
-    else:
+        tree = ET.parse(xmlFilePath)
+        # 获得根节点
+        root = tree.getroot()
         for child in root:
-            if child.tag == 'object' and child[0].text != 'sheep':
-                # 移除节点
-                root.remove(child)
-        # 保存
-        tree.write(xmlFilePath)
+            if child.tag == 'filename':
+                jpgName = child.text
+            if child.tag == 'object' and child[0].text == 'car':
+                flag = False
 
+        if flag:
+            print('不存在 car标注，删除 %s 和 对应的图片%s' % (xmlFilePath, jpgName))
+
+            os.remove(xmlFilePath)
+            os.remove(os.path.join(jpgPath, jpgName))
+
+        else:
+            for child in root:
+                if child.tag == 'object' and child[0].text != 'car':
+                    # 移除节点
+                    root.remove(child)
+                    # 保存
+                    tree.write(xmlFilePath)
+    except Exception as e:
+        print(e)
 
 def traversalXml(xmlPath, jpgPath):
     for filename in os.listdir(xmlPath):
