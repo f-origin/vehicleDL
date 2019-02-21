db247572b8913a7fa1f1ec1800f2ac5d50c34c21
origin <f_origin@163.com>
2019-02-21 22:04:18 +0800

    update

--- code/run.sh
+++ code/run.sh
@@ -9,13 +9,16 @@ echo current dir is $PWD
 export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
-output_dir=/output  # 训练目录
-dataset_dir=/data/forigin/my-object # 数据集目录，这里是写死的，记得修改
+#output_dir=/output  # 训练目录
+#dataset_dir=/data/forigin/my-object # 数据集目录，这里是写死的，记得修改
+
+output_dir=/home/david/tmp/voc-model  # 训练目录
+dataset_dir=/home/david/tmp/voc-model/data # 数据集目录，这里是写死的，记得修改
 
 # config文件??
 PIPELINE_CONFIG_PATH=${dataset_dir}/ssd_mobilenet_v1_my.config
 MODEL_DIR=${output_dir}/model
-NUM_TRAIN_STEPS=500
+NUM_TRAIN_STEPS=50
 SAMPLE_1_OF_N_EVAL_EXAMPLES=10
 
 python3 object_detection/model_main.py \
